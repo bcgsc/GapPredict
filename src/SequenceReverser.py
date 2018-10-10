6d303467c9fa3f2f1fd967015914064688600daa
Eric Chen <echen@bcgsc.ca>
2018-10-10 10:46:45 -0700

    Implement Sequence Reverser
    Implement a class that takes the reverse complement of a
    sequence and its quality (since technically any respective
    base on a strand and its calculated reverse complement
    should be equally probable to be incorrect since the reverse
    complement was calculated)

---
+++ src/SequenceReverser.py
@@ -0,0 +1,24 @@
+from models.ParsedFastqRecord import ParsedFastqRecord
+
+COMPLEMENT_MAP = {
+    "A": "T",
+    "T": "A",
+    "G": "C",
+    "C": "G"
+}
+
+class SequenceReverser:
+    def __init__(self):
+        pass
+
+    def reverse_complement(self, sequence):
+        reverse_seq = ""
+        for i in range(len(sequence)):
+            reverse_idx = len(sequence) - i - 1
+            reverse_seq += COMPLEMENT_MAP[sequence[reverse_idx]]
+        return reverse_seq
+
+    def reverse_sequence(self, parsed_fastq):
+        reverse_complement = self.reverse_complement(parsed_fastq.sequence)
+        reverse_quality = parsed_fastq.phred_quality[::-1]
+        return ParsedFastqRecord(reverse_complement, reverse_quality)
---
+++ test/test_sequence_reverser.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from SequenceReverser import SequenceReverser
+from models.ParsedFastqRecord import ParsedFastqRecord
+
+
+class TestSequenceReverser(TestCase):
+    def setUp(self):
+        self.reverser = SequenceReverser()
+
+    def test_reverse_complement(self):
+        reverse_complement = self.reverser.reverse_complement("ATGCCGT")
+        self.assertEqual(reverse_complement, "ACGGCAT")
+
+    def test_reverse_complement_empty(self):
+        reverse_complement = self.reverser.reverse_complement("")
+        self.assertEqual(reverse_complement, "")
+
+    def test_reverse_sequence(self):
+        sequence = ParsedFastqRecord("ATGCCGT", [10, 11, 12, 13, 14, 15, 16])
+        reverse_sequence = self.reverser.reverse_sequence(sequence)
+        expected_reverse_sequence = ParsedFastqRecord("ACGGCAT", [16, 15, 14, 13, 12, 11, 10])
+        self.assertEqual(reverse_sequence, expected_reverse_sequence)
+
+    def test_reverse_sequence_empty(self):
+        sequence = ParsedFastqRecord("", [])
+        reverse_sequence = self.reverser.reverse_sequence(sequence)
+        expected_reverse_sequence = ParsedFastqRecord("", [])
+        self.assertEqual(reverse_sequence, expected_reverse_sequence)
+
+    def test_reverse_sequence_palindrome(self):
+        sequence = ParsedFastqRecord("ATGCAT", [10, 11, 12, 13, 14, 15])
+        reverse_sequence = self.reverser.reverse_sequence(sequence)
+        expected_reverse_sequence = ParsedFastqRecord("ATGCAT", [15, 14, 13, 12, 11, 10])
+        self.assertEqual(reverse_sequence, expected_reverse_sequence)
