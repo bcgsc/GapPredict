827779436316a000851c0bba9f541a720b7c444a
echen424 <l0z9a@ugrad.cs.ubc.ca>
2018-10-14 22:43:58 -0700

    Allow choice of a start index for sequence matching
    We can now choose where in the sequence we want to
    start
    
    This might be useful if we care about only a specific
    substring of a sequence

--- src/SequenceMatchCalculator.py
+++ src/SequenceMatchCalculator.py
@@ -2,11 +2,17 @@ class SequenceMatchCalculator:
     def __init__(self):
         pass
 
-    def compare_sequences(self, seq1, seq2):
+    def compare_sequences(self, seq1, seq2, start_idx=0, bases_to_check=None):
         #TODO: assumes equal length
         mismatches = 0
-        for i in range(len(seq1)):
-            if seq1[i] != seq2[i]:
+        length = len(seq1) - max(start_idx, 0)
+
+        if bases_to_check != None:
+            length = min(len(seq1) - start_idx, max(bases_to_check, 0))
+
+        for i in range(length):
+            idx = i + start_idx
+            if seq1[idx] != seq2[idx]:
                 mismatches += 1
         return mismatches
 
