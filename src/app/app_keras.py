ba4d1a87173c3274caa4d25b5d18e18a3227d029
Eric Chen <echen@bcgsc.ca>
2018-11-05 11:06:39 -0800

    Extra flag

--- src/app/app_keras.py
+++ src/app/app_keras.py
@@ -17,7 +17,7 @@ def get_stats(inputs, outputs):
     freq_map.load_input_outputs(inputs, outputs)
     return freq_map
 
-def extract_read_matrix(paths, input_length, spacing, bases_to_predict, include_reverse_complement):
+def extract_read_matrix(paths, input_length, spacing, bases_to_predict, include_reverse_complement, unique):
     importer = SequenceImporter()
     extractor = SlidingWindowExtractor(input_length, spacing, bases_to_predict)
     encoder = KmerLabelEncoder()
@@ -28,7 +28,7 @@ def extract_read_matrix(paths, input_length, spacing, bases_to_predict, include_
     print("Import took " + str(end_time - start_time) + "s")
 
     start_time = time.clock()
-    input_kmers, output_kmers, quality_vectors = extractor.extract_kmers_from_sequence(reads)
+    input_kmers, output_kmers, quality_vectors = extractor.extract_kmers_from_sequence(reads, unique=unique)
     end_time = time.clock()
     print("Extraction took " + str(end_time - start_time) + "s")
 
@@ -112,12 +112,13 @@ def main():
     spacing = 0
     k = 1
     has_quality = False
+    unique = False
 
     match_calculator = SequenceMatchCalculator()
 
     paths = ['data/read_1_300.fastq', 'data/read_2_300.fastq']
     input_seq, input_quality, output_seq, shifted_output_seq, input_stats_map = extract_read_matrix(paths, input_length, spacing,
-                                                                                   bases_to_predict, include_reverse_complement=include_reverse_complement)
+                                                                                   bases_to_predict, include_reverse_complement, unique)
     #TODO: kind of long...
     input_seq_train, input_seq_valid, input_quality_train, input_quality_valid, output_seq_train, output_seq_valid, shifted_output_train, shifted_output_valid = model_selection.train_test_split(input_seq, input_quality, output_seq, shifted_output_seq, test_size=0.15, random_state=123)
     print("Encoding training set")
@@ -126,7 +127,7 @@ def main():
     input_one_hot_cube_valid, output_one_hot_cube_valid, shifted_output_seq_cube_valid = encode_reads(input_length, bases_to_predict, input_seq_valid, input_quality_valid, output_seq_valid, shifted_output_valid, has_quality=has_quality)
 
     output_decoder = OneHotMatrixDecoder(bases_to_predict)
-    model = KerasRNNModel(has_quality=has_quality, prediction_length=k, batch_size=64, epochs=5, latent_dim=100)
+    model = KerasRNNModel(has_quality=has_quality, prediction_length=k, batch_size=64, epochs=10, latent_dim=100)
 
     start_time = time.clock()
     model.fit(input_one_hot_cube_train, output_one_hot_cube_train, shifted_output_seq_cube_train)
