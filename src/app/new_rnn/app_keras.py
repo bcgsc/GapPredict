2c034072960d33ab0245b11bb05b27ea46ba76d9
Eric Chen <echen@bcgsc.ca>
2019-02-06 09:02:55 -0800

    graphs

--- src/app/new_rnn/app_keras.py
+++ src/app/new_rnn/app_keras.py
@@ -9,8 +9,8 @@ from predict.new_rnn.SingleLSTMModel import SingleLSTMModel
 from preprocess.SequenceImporter import SequenceImporter
 
 def _plot_training_validation(epochs, validation_metrics, training_accuracy):
-    root_path = 'E:\\Users\\Documents\\School Year 18-19\\Term 1\\CPSC 449\\Sealer_NN\\src\\app\\new_rnn\\out\\'
-    #root_path = '/home/echen/Desktop/Projects/Sealer_NN/src/app/new_rnn/out/'
+    #root_path = 'E:\\Users\\Documents\\School Year 18-19\\Term 1\\CPSC 449\\Sealer_NN\\src\\app\\new_rnn\\out\\'
+    root_path = '/home/echen/Desktop/Projects/Sealer_NN/src/app/new_rnn/out/'
 
     plt.figure()
     plt.ylim(0, 1.1)
@@ -41,7 +41,7 @@ def main():
     path = '../data/ecoli_contigs/ecoli_contig_1000.fasta'
     reference_sequence = importer.import_fasta([path])[0]
 
-    model = SingleLSTMModel(min_seed_length=min_seed_length, spacing=spacing, stateful=False, batch_size=128, epochs=100, embedding_dim=25, latent_dim=100, with_gpu=True, log_samples=True, reference_sequence=reference_sequence)
+    model = SingleLSTMModel(min_seed_length=min_seed_length, spacing=spacing, stateful=False, batch_size=128, epochs=1000, embedding_dim=25, latent_dim=100, with_gpu=True, log_samples=True, reference_sequence=reference_sequence)
 
     start_time = time.time()
     history = model.fit(reads)
