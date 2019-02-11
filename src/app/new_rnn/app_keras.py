7b32c1f5c980f2b035ed662270322e8c4edc9423
Eric Chen <echen@bcgsc.ca>
2019-02-11 10:20:55 -0800

    Implement hyperparameter iteration
    If we only want a specific set of hyperparameters just set the
    array to size 1

--- src/app/new_rnn/app_keras.py
+++ src/app/new_rnn/app_keras.py
@@ -9,11 +9,16 @@ import os
 from predict.new_rnn.SingleLSTMModel import SingleLSTMModel
 from preprocess.SequenceImporter import SequenceImporter
 
-def _plot_training_validation(epochs, validation_metrics, training_accuracy):
+def _plot_training_validation(epochs, validation_metrics, training_accuracy, directory_name):
     if os.name == 'nt':
-        root_path = 'E:\\Users\\Documents\\School Year 18-19\\Term 1\\CPSC 449\\Sealer_NN\\src\\app\\new_rnn\\out\\'
+        root_path = 'E:\\Users\\Documents\\School Year 18-19\\Term 1\\CPSC 449\\Sealer_NN\\src\\app\\new_rnn\\out\\training_metrics\\'
+        root_path += directory_name + '\\'
     else:
-        root_path = '/home/echen/Desktop/Projects/Sealer_NN/src/app/new_rnn/out/'
+        root_path = '/home/echen/Desktop/Projects/Sealer_NN/src/app/new_rnn/out/training_metrics/'
+        root_path += directory_name + "/"
+
+    if not os.path.exists(root_path):
+        os.makedirs(root_path)
 
     plt.figure(figsize=(24, 18))
     plt.ylim(0, 1.1)
@@ -34,7 +39,7 @@ def _plot_training_validation(epochs, validation_metrics, training_accuracy):
 def main():
     include_reverse_complement = True
     min_seed_length = 26
-    spacing = 10
+    spacing = 0
 
     arguments = sys.argv[1:]
     paths = arguments if len(arguments) > 0 else ['../data/ecoli_contigs/ecoli_contig_1000.fastq']
@@ -44,18 +49,66 @@ def main():
     path = '../data/ecoli_contigs/ecoli_contig_1000.fasta'
     reference_sequence = importer.import_fasta([path])[0]
 
-    model = SingleLSTMModel(min_seed_length=min_seed_length, spacing=spacing, stateful=False, batch_size=128, epochs=100, embedding_dim=25, latent_dim=100, with_gpu=True, log_samples=True, reference_sequence=reference_sequence)
+    with_gpu=True
+    log_samples=False
+    epochs = 1000
+    replicates = 2
 
-    start_time = time.time()
-    history = model.fit(reads)
-    model.save_weights('../weights/my_model_weights.h5')
-    end_time = time.time()
-    print("Fitting took " + str(end_time - start_time) + "s")
+    # (128, 1024, 1024)
+    # doubling latent_dim seems to increase # parameters by ~3X
+    # doubling embedding_dim seems to increase # parameters by ~1.5X
+    #batch_size, embedding_dim, latent_dim
+    hyperparameters = [
+        (64, 32, 32),
+        (64, 32, 128),
+        (64, 32, 512),
+        (64, 128, 32),
+        (64, 128, 128),
+        (64, 128, 512),
+        (64, 512, 32),
+        (64, 512, 128),
+        (64, 512, 512),
+        (128, 32, 32),
+        (128, 32, 128),
+        (128, 32, 512),
+        (128, 128, 32),
+        (128, 128, 128),
+        (128, 128, 512),
+        (128, 512, 32),
+        (128, 512, 128),
+        (128, 512, 512),
+        (256, 32, 32),
+        (256, 32, 128),
+        (256, 32, 512),
+        (256, 128, 32),
+        (256, 128, 128),
+        (256, 128, 512),
+        (256, 512, 32),
+        (256, 512, 128),
+        (256, 512, 512)
+    ]
+    for hyperparameter in hyperparameters:
+        batch_size = hyperparameter[0]
+        embedding_dim = hyperparameter[1]
+        latent_dim = hyperparameter[2]
 
-    epochs = np.array(history.epoch)
-    validation_metrics = model.validation_history()[0]
-    training_accuracy = np.array(history.history['acc'])
-    _plot_training_validation(epochs, validation_metrics, training_accuracy)
+        for i in range(replicates):
+            model = SingleLSTMModel(min_seed_length=min_seed_length, spacing=spacing, stateful=False, batch_size=batch_size,
+                                    epochs=epochs, embedding_dim=embedding_dim, latent_dim=latent_dim, with_gpu=with_gpu,
+                                    log_samples=log_samples, reference_sequence=reference_sequence)
+
+            start_time = time.time()
+            history = model.fit(reads)
+            model.save_weights('../weights/my_model_weights.h5')
+            end_time = time.time()
+            print("Fitting took " + str(end_time - start_time) + "s")
+
+            epoch_axis = np.array(history.epoch)
+            validation_metrics = model.validation_history()[0]
+            training_accuracy = np.array(history.history['acc'])
+            directory_name = "BS_" + str(batch_size) + "_ED_" + str(embedding_dim) + "_LD_" + str(latent_dim) \
+                             + "_E_" + str(epochs) + "_R_" + str(i)
+            _plot_training_validation(epoch_axis, validation_metrics, training_accuracy, directory_name)
 
 if __name__ == "__main__":
     main()
