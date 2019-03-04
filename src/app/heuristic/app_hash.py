709c98eb698a2f9465f7d2c90aa89d80cf74ba35
Eric Chen <echen@bcgsc.ca>
2019-03-04 11:10:34 -0800

    Full pipeline for the hashmap prediction application
    This is a heuristic prediction now being used to predict gaps as a
    benchmark

--- src/app/heuristic/app_hash.py
+++ src/app/heuristic/app_hash.py
@@ -14,15 +14,13 @@ from preprocess.SequenceImporter import SequenceImporter
 from preprocess.SequenceReverser import SequenceReverser
 from viz.SequenceRegenerationViz import SequenceRegenerationViz
 
-def main():
+def hash_predict(seed_length, length_to_predict, fastq_path, fasta_path):
     include_reverse_complement = True
-    seed_length = 50
     bases_to_predict = 1
-    length_to_predict = 750
     spacing = 0
     unique = False
 
-    training_paths = ['../data/real_gaps/sealer_filled/7465348_13506-14596.fastq']
+    training_paths = [fastq_path]
     input_kmers_train, output_kmers_train = helper.extract_kmers(training_paths, seed_length,
                                                                                         spacing, bases_to_predict,
                                                                                         include_reverse_complement,
@@ -31,6 +29,15 @@ def main():
     validator = SequenceMatchCalculator()
     reverser = SequenceReverser()
     model = HashModel(bases_to_predict)
+
+    if os.name == 'nt':
+        root_path = 'E:\\Users\\Documents\\School Year 18-19\\Term 1\\CPSC 449\\Sealer_NN\\src\\app\\heuristic\\out\\'
+    else:
+        root_path = '/home/echen/Desktop/Projects/Sealer_NN/src/app/heuristic/out/'
+
+    terminal_directory_character = UTILS.get_terminal_directory_character()
+    id = fasta_path.split(terminal_directory_character)[-1].split(".")[0] + "_SL_" + str(seed_length)
+    viz = SequenceRegenerationViz(root_directory=root_path, directory=id)
 
     start_time = time.time()
     model.fit(input_kmers_train, output_kmers_train)
@@ -51,9 +58,6 @@ def main():
 
     # FASTA prediction
     importer = SequenceImporter()
-    fasta_path = '../data/real_gaps/sealer_filled/7465348_13506-14596.fasta'
-    terminal_directory_character = UTILS.get_terminal_directory_character()
-    id = fasta_path.split(terminal_directory_character)[-1].split(".")[0]
 
     sequences = importer.import_fasta([fasta_path])
     left_flank = sequences[0]
@@ -61,20 +65,20 @@ def main():
     right_flank = sequences[1]
     right_flank_rc = reverser.reverse_complement(right_flank)
 
-    predict(left_flank, model, seed_length)
-    predict(left_flank_rc, model, seed_length)
-    predict(right_flank, model, seed_length)
-    predict(right_flank_rc, model, seed_length)
+    lfp, lfp_matches = predict(left_flank, model, seed_length)
+    lfprc, lfprc_matches = predict(left_flank_rc, model, seed_length)
+    rfp, rfp_matches = predict(right_flank, model, seed_length)
+    rfprc, rfprc_matches = predict(right_flank_rc, model, seed_length)
+
+    viz.compare_sequences(left_flank, lfp, seed_length, lfp_matches)
+    viz.compare_sequences(left_flank_rc, lfprc, seed_length, lfprc_matches, append=True)
+    viz.compare_sequences(right_flank, rfp, seed_length, rfp_matches, append=True)
+    viz.compare_sequences(right_flank_rc, rfprc, seed_length, rfprc_matches, append=True)
 
     forward = predict_arbitrary(left_flank, model, seed_length, length_to_predict)
     reverse_complement = predict_arbitrary(right_flank_rc, model, seed_length, length_to_predict)
 
-    if os.name == 'nt':
-        root_path = 'E:\\Users\\Documents\\School Year 18-19\\Term 1\\CPSC 449\\Sealer_NN\\src\\app\\heuristic\\out\\'
-    else:
-        root_path = '/home/echen/Desktop/Projects/Sealer_NN/src/app/heuristic/out/'
-    viz = SequenceRegenerationViz(root_directory=root_path, directory=id)
-    viz.save_complements(forward, reverse_complement, fasta_ref=fasta_path)
+    viz.save_complements(forward, reverse_complement, id=id, fasta_ref=fasta_path)
 
 def predict(sequence, model, seed_length):
     validator = SequenceMatchCalculator()
@@ -100,8 +104,7 @@ def predict(sequence, model, seed_length):
 
     predicted_sequence = current_sequence[seed_length:sequence_length]
     matches = validator.compare_sequences(predicted_sequence, bases_to_predict)
-    mean_match = np.mean(matches)
-    print(mean_match)
+    return current_sequence, matches
 
 def predict_arbitrary(flank, model, seed_length, length_to_predict):
     remaining_length = length_to_predict
@@ -120,6 +123,17 @@ def predict_arbitrary(flank, model, seed_length, length_to_predict):
         upper_bound += 1
 
     return current_sequence
+
+def main():
+    seed_lengths = [26, 52, 104]
+    lengths_to_predict = [750]
+    ids = ["7465325_7588-8680", "7465348_13506-14596", "7473236_14236-15319", "7473263_4934-6078", "7505257_43822-44849"]
+    for seed_length in seed_lengths:
+        for length_to_predict in lengths_to_predict:
+            for file_id in ids:
+                fastq_path = '../data/real_gaps/sealer_filled/' + file_id + '.fastq'
+                fasta_path = '../data/real_gaps/sealer_filled/' + file_id + '.fasta'
+                hash_predict(seed_length, length_to_predict, fastq_path, fasta_path)
 
 if __name__ == "__main__":
     main()
