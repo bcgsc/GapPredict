b4ce32b17e8ae0e31a6117b4129a2a66632b7324
Eric Chen <echen@bcgsc.ca>
2019-03-26 12:06:42 -0700

    Flank predictions

--- src/app/helper_scripts/10_predict_flanks.py
+++ src/app/helper_scripts/10_predict_flanks.py
@@ -8,6 +8,12 @@ else:
 
 import utils.directory_utils as dir_utils
 from app.new_rnn.predict_by_reference import predict_reference
+from keras import backend as K
+import tensorflow as tf
+
+def reset_states():
+    K.clear_session()
+    tf.reset_default_graph()
 
 def main():
     if os.name == 'nt':
@@ -21,16 +27,19 @@ def main():
 
     dir_utils.mkdir(output_folder)
 
-    lstm_cell_directories = os.listdir(root)
-    replicates = 10
+    sets = os.listdir(root)
+    replicates = 30
 
     ids = set()
-    for lstm_cells in lstm_cell_directories:
-        dim_path = root + lstm_cells + terminal_char
-        experiments = os.listdir(dim_path)
-        for id in experiments:
-            fasta_id = id.split("_R_")[0]
-            ids.add(fasta_id)
+    for set_num in sets:
+        cwd = root + set_num + terminal_char
+        lstm_cell_directories = os.listdir(cwd)
+        for lstm_cells in lstm_cell_directories:
+            dim_path = cwd + lstm_cells + terminal_char
+            experiments = os.listdir(dim_path)
+            for id in experiments:
+                fasta_id = id.split("_R_")[0]
+                ids.add(fasta_id)
 
     if os.name == 'nt':
         fasta_path = 'E:\\Users\\Documents\\School Year 18-19\\Term 1\\CPSC 449\\Sealer_NN\\src\\app\\data\\real_gaps\\sealer_filled\\7391826_358-1408.fasta'
@@ -40,17 +49,21 @@ def main():
     embedding_dim = 128
 
     for id in ids:
-        for lstm_cells in lstm_cell_directories:
-            for i in range(replicates):
-                folder = id + "_R_" + str(i)
-                folder_path = root + lstm_cells + terminal_char + folder + terminal_char
-                weights_path = folder_path
+        for set_num in sets:
+            cwd = root + set_num + terminal_char
+            lstm_cell_directories = os.listdir(cwd)
+            for lstm_cells in lstm_cell_directories:
+                for i in range(replicates):
+                    folder = id + "_R_" + str(i)
+                    folder_path = cwd + lstm_cells + terminal_char + folder + terminal_char
+                    weights_path = folder_path
 
-                tokens = lstm_cells.split("_")
-                latent_dim = int(tokens[0])
-                min_seed_length = int(tokens[2])
+                    tokens = lstm_cells.split("_")
+                    latent_dim = int(tokens[0])
+                    min_seed_length = int(tokens[2])
 
-                predict_reference(weights_path, fasta_path, embedding_dim, latent_dim, min_seed_length, id, base_path=folder_path)
+                    predict_reference(weights_path, fasta_path, embedding_dim, latent_dim, min_seed_length, id, base_path=folder_path)
+                    reset_states()
 
 if __name__ == "__main__":
     main()
