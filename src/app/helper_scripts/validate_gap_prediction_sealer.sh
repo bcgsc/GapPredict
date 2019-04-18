3a8c8d845505ee9cce745e80d63f01504525f174
Eric Chen <echen@bcgsc.ca>
2019-04-18 12:12:36 -0700

    Get metrics on greedy algorithm as well
    Add a total length column for our % identity

--- src/app/helper_scripts/validate_gap_prediction_sealer.sh
+++ src/app/helper_scripts/validate_gap_prediction_sealer.sh
@@ -18,7 +18,7 @@ for model in $all_models/*/; do
     out_path=$valid_dir/$gap_id
     mkdir -p $out_path
     
-    exonerate $sealer_filled $gap_reference --ryo "percent_identity:%pi\nquery_start:%qab\nquery_length:%ql\nquery_alignment_length:%qal\nquery_end:%qae\ntarget_start:%tab\ntarget_end:%tae\ntarget_length:%tl\ntarget_alignment_length:%tal\ntotal_bases_compared:%et\nmismatches:%em\nmatches:%ei\n" --model affine:local --exhaustive y --bestn 1 > $out_path/sealer.exn
-    exonerate $sealer_merged $gap_reference --ryo "percent_identity:%pi\nquery_start:%qab\nquery_length:%ql\nquery_alignment_length:%qal\nquery_end:%qae\ntarget_start:%tab\ntarget_end:%tae\ntarget_length:%tl\ntarget_alignment_length:%tal\ntotal_bases_compared:%et\nmismatches:%em\nmatches:%ei\n" --model affine:local --exhaustive y --bestn 1 > $out_path/sealer_merged.exn
+    exonerate $sealer_filled $gap_reference --ryo "percent_identity:%pi\nquery_start:%qab\nquery_length:%ql\nquery_alignment_length:%qal\nquery_end:%qae\ntarget_start:%tab\ntarget_end:%tae\ntarget_length:%tl\ntarget_alignment_length:%tal\ntotal_bases_compared:%et\nmismatches:%em\nmatches:%ei\ncigar:%C\n" --model affine:local --exhaustive y --bestn 1 > $out_path/sealer.exn
+    exonerate $sealer_merged $gap_reference --ryo "percent_identity:%pi\nquery_start:%qab\nquery_length:%ql\nquery_alignment_length:%qal\nquery_end:%qae\ntarget_start:%tab\ntarget_end:%tae\ntarget_length:%tl\ntarget_alignment_length:%tal\ntotal_bases_compared:%et\nmismatches:%em\nmatches:%ei\ncigar:%C\n" --model affine:local --exhaustive y --bestn 1 > $out_path/sealer_merged.exn
   fi  
 done
