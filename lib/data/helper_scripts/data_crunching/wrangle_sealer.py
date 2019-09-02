a5013b80250599370d7b6ff26e475e6defe20b4b
echen424 <l0z9a@ugrad.cs.ubc.ca>
2019-09-01 20:37:45 -0700

    Improve figures
    1) Better axis labels
    2) Colour bar for probability density
    3) Thicker lines
    4) More clear contours

--- lib/data/helper_scripts/data_crunching/wrangle_sealer.py
+++ lib/data/helper_scripts/data_crunching/wrangle_sealer.py
@@ -1,5 +1,6 @@
 import os
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -7,10 +8,15 @@ import seaborn as sns
 
 import utils.directory_utils as utils
 
-if os.name == 'nt':
-    base_path = "E:\\Users\\Documents\\School Year 18-19\\Term 1\\CPSC 449\\Sealer_NN\\lib\\app\\helper_scripts\\data_crunching\\"
-else:
-    base_path = "/home/echen/Desktop/Projects/Sealer_NN/lib/app/helper_scripts/data_crunching/"
+base_path = "E:\\Users\\Documents\\School_Year_18-19\\Term_1\\CPSC_449\\Sealer_NN\\lib\\data\\helper_scripts\\data_crunching\\"
+
+axiswidth = 3
+ticksize = 15
+mpl.rcParams['axes.linewidth'] = axiswidth
+mpl.rcParams['xtick.major.size'] = ticksize
+mpl.rcParams['xtick.major.width'] = axiswidth
+mpl.rcParams['ytick.major.size'] = ticksize
+mpl.rcParams['ytick.major.width'] = axiswidth
 
 def transform_dataframe(df):
     df['total_percent_id'] = (df.matches/df.total_length).fillna(0)
@@ -18,10 +24,10 @@ def transform_dataframe(df):
     df['target_correctness'] = (df.total_percent_id * df.coverage * 100).fillna(0)
 
 def plot_box(data, file_path):
-    primary_text_font_size = 45
-    secondary_text_font_size = 35
+    primary_text_font_size = 55
+    secondary_text_font_size = 45
     linewidth = 6
-    rotation = 45
+    rotation = 0
     plt.rc('xtick', labelsize=secondary_text_font_size)
     plt.rc('ytick', labelsize=secondary_text_font_size)
     font = {
@@ -41,9 +47,9 @@ def plot_box(data, file_path):
 
     ax = sns.boxplot(data=data, x="fixed_status", y="target_correctness", linewidth=linewidth, flierprops=flierprops)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=rotation)
-    plt.ylim((-5, 100))
-    plt.xlabel('filled_with_all_reads')
-    plt.ylabel('target % correctness')
+    plt.ylim((-5, 101))
+    plt.xlabel('Gap Reads Source')
+    plt.ylabel('Target Correctness (%)')
 
     plt.tight_layout()
     fig = plt.savefig(file_path)
@@ -61,10 +67,10 @@ fixed = columns_to_plot[(columns_to_plot.is_fixed == 1)]
 unfixed = columns_to_plot[(columns_to_plot.is_fixed == 0)]
 joined_fixed = pd.merge(fixed, gap_fill_df, on=["gap_id", "is_fixed"], how="right")
 joined_fixed = joined_fixed[~np.isnan(joined_fixed.target_correctness)]
-joined_fixed["fixed_status"] = "filled"
+joined_fixed["fixed_status"] = "All"
 joined_unfixed = pd.merge(unfixed, gap_fill_df, on=["gap_id", "is_fixed"], how="right")
 joined_unfixed = joined_unfixed[~np.isnan(joined_unfixed.target_correctness)]
-joined_unfixed["fixed_status"] = "unfilled"
+joined_unfixed["fixed_status"] = "Individual"
 full_data = joined_fixed.append(joined_unfixed, ignore_index=True)
 
 delimiter = utils.get_terminal_directory_character()
